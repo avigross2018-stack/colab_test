53449864b0908bbd525b72720b22116b2b68b68e
Avi Gross <avigross2018@gmail.com>
2026-02-02 11:23:46 +0200

    added-sub-power-root-func

---
+++ operators_func.py
@@ -0,0 +1,26 @@
+def sub(a,b):
+    '''
+    Docstring for sub
+    subtract a and b
+    
+    :param a: number
+    :param b: number
+    '''
+    return a - b
+
+def power(a,b):
+    '''
+    Docstring for power
+    a in power of b
+    :param a: number
+    :param b: number
+    '''
+    return a ** b
+
+def root(a):
+    '''
+    Docstring for root
+    a in power of (0.5)
+    :param a: int number
+    '''
+    return a ** 0.5
