56f6608f416e9c034588b234b27aee9c7e5880d1
Yehuda <yehudadarmon@gmail.com>
2026-02-02 14:15:40 +0200

    added Geometric formulas

--- operators_func.py
+++ operators_func.py
@@ -5,7 +5,11 @@ def multi(num_1,num_2):
 
 # This function divides two numbers
 def divide(num_1,num_2):
-    return num_1 / num_2
+    try:
+        result = num_1 / num_2
+        return result
+    except ZeroDivisionError:
+        return "Error: You cannot divide by zero! 😅"
 
 
 # This function adds two numbers together
@@ -40,10 +44,35 @@ def root(a):
     '''
     return a ** 0.5
 
-
+# This function return the absolute value
 def absolute_value(number):
     if number < 0:
         return - number
     else:
         return number
     
+    
+# This function returns the area of ​​a triangle
+def triangle_area(num_1,num_2):
+    return num_1 * num_2 / 2
+
+
+# This function returns the area of ​​a square
+def square_area(number):
+    return number * 2
+
+
+# This function returns the area of ​​a rectangle
+def rectangle_area(num_1,num_2):
+    return num_1 * num_2
+
+
+# This function returns the area of ​​a circle
+import math
+
+def circle_area(radius):
+    if radius < 0:
+        return "radius cannot be negative!"
+    
+    return math.pi * (radius ** 2)
+
