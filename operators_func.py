b5ed9fdbae9446dfb01e16b5d664f809f8962349
Yehuda <yehudadarmon@gmail.com>
2026-02-02 11:26:32 +0200

    added funcs multi, divide, add

---
+++ operators_func.py
@@ -0,0 +1,15 @@
+# This function adds two numbers together
+def multi(num_1,num_2):
+    return num_1 * num_2
+
+
+# This function divides two numbers
+def divide(num_1,num_2):
+    return num_1 / num_2
+
+
+# This function adds two numbers together
+def add(num_1,num_2):
+    return num_1 + num_2
+
+
